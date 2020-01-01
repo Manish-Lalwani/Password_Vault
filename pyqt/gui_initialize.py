4e39523bda0da4136ab32871ca68d44c02837698
Manish-Lalwani <54809501+Manish-Lalwani@users.noreply.github.com>
2020-01-02 03:30:34 +0530

    Update gui_initialize.py

--- pyqt/gui_initialize.py
+++ pyqt/gui_initialize.py
@@ -2,9 +2,16 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 
+class GUIApp:
+	def __init__(self,ui_filepath):
+		self.ui_filepath = ui_filepath
 
-app = QApplication([])
-dlg = uic.loadUi("gui.ui")
+		app = QApplication([])
+		dlg = uic.loadUi(self.ui_filepath)
+		dlg.show()
+		app.exec()
 
-dlg.show()
-app.exec()
+
+
+if __name__ == "__main__":
+	gui_obj = GUIApp("gui.ui")
